04ef022ea6da534cbdea072f116fa68e22c190ec
KonstBeliakov <konstbeliakov@gmail.com>
2024-05-02 13:26:46 +0300

    refactoring

--- main.py
+++ main.py
@@ -3,17 +3,44 @@ import pygame
 from math import sin, cos, pi
 from time import perf_counter
 
-noiseGenerator = PerlinNoise()
-r = 100
-height = [r + noiseGenerator(i * 0.05) * 100 for i in range(0, 255)]
-k = height[0] / height[-1]
-height = [height[i] * (1 + (k - 1) * (i / 255)) for i in range(len(height))]
-
 
 def position(h, alpha):
+    """
+    :param h: list of distances from the center of a figure to points on its surface
+    :param alpha: angle of rotation of the figure
+    :return: coordinates of points on the surface of a figure relative to its center
+    """
     angle = [2 * pi * i / len(h) for i in range(len(h))]
-    return [[400 + height[i] * sin(a + alpha), 400 + h[i] * cos(a + alpha)] for i, a in enumerate(angle)]
+    return [[h[i] * sin(a + alpha), h[i] * cos(a + alpha)] for i, a in enumerate(angle)]
 
+
+def draw(center_position, screen, pos, alpha, stretch=1.0):
+    """
+    :param center_position: position of center of rotating figure
+    :param screen: pygame screen
+    :param pos: coordinates of points on the surface of a figure relative to its center
+    :param alpha: rotation angle of the construction
+    :param stretch: surface tensile coefficient
+    :return: None
+    """
+    for i in range(len(pos) - 1):
+        color = (0, 0, 0)  # (255 - i, i, 0)
+        pygame.draw.line(screen, color, [pos[i][0] + center_position[0], pos[i][1] + center_position[1]],
+                         [pos[i + 1][0] + center_position[0], pos[i + 1][1] + center_position[1]], 1)
+    dx = r * alpha
+
+    for i in range(len(height) - 1):
+        color = (0, 0, 0)  # (0, i, 255)
+        pygame.draw.line(screen, color, [-dx * stretch + i * stretch * (2 * pi * r / n) + center_position[0], height[i] + center_position[1]],
+                         [-dx * stretch + i * stretch * (2 * pi * r / n) + 1 + center_position[0], height[i + 1] + center_position[1]], 1)
+
+
+noiseGenerator = PerlinNoise()
+r = 100
+n = 255
+height = [r + noiseGenerator(i * 0.05) * 100 for i in range(n)]
+k = height[0] / height[-1]
+height = [height[i] * (1 + (k - 1) * (i / n)) for i in range(len(height))]
 
 pygame.init()
 
@@ -30,16 +57,13 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    screen.fill([0, 0, 0])
+    screen.fill([255, 255, 255])
     alpha = (perf_counter() - t) * t_speed
     pos = position(height, -alpha)
-    for i in range(len(pos) - 1):
-        pygame.draw.line(screen, (255 - i, i, 0), pos[i], pos[i + 1], 1)
-    dx = r * alpha
 
-    for i in range(len(height) - 1):
-        pygame.draw.line(screen, (0, i, 255), [-dx + 400 + i * (2 * pi * r / 255), 400 + height[i]],
-                         [-dx + 400 + i * (2 * pi * r / 255) + 1, 400 + height[i + 1]], 1)
+    draw([100, 100], screen, pos, alpha,  1.0)
+
+    draw([100, 500], screen, pos, alpha, 1.1)
 
     pygame.display.flip()
 pygame.quit()
