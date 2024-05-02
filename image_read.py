a935bdfa74fee571bbb3b0f9f0764cbe1476982d
KonstBeliakov <konstbeliakov@gmail.com>
2024-05-03 01:31:30 +0300

    added the ability to load a gear shape from a png file

---
+++ image_read.py
@@ -0,0 +1,65 @@
+import cv2
+import copy
+import math
+import pygame
+
+
+def dist(x1, y1, x2, y2):
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** .5
+
+
+img = cv2.imread('image.png')
+img2 = copy.deepcopy(img)
+
+l = []
+
+minX = img.shape[1]
+maxX = 0
+minY = img.shape[0]
+maxY = 0
+
+for i in range(1, len(img) - 1):
+    for j in range(1, len(img[i]) - 1):
+        if img[i][j][0] == 0:
+            minX = min(minX, j)
+            maxX = max(maxX, j)
+            minY = min(minY, i)
+            maxY = max(maxY, i)
+        if img[i + 1][j][0] == 0 and img[i - 1][j][0] == 0 and img[i][j - 1][0] == 0 and img[i][j + 1][1] == 0:#and img[i + 1][j][1] == 0 and img[i + 1][j][2] == 2: #all([sum(i) == 0 for i in [img[i - 1][j], img[i + 1][j], img[i][j - 1], img[i][j + 1]]]):
+            img2[i][j] = [255, 255, 255]
+        elif img[i][j][0] == 0:
+            img2[i][j] = [0, 0, 0]
+            l.append([i, j])
+        else:
+            img2[i][j] = [255, 255, 255]
+
+center = [(minY + maxY) // 2, (minX + maxX) // 2]
+r = min((maxX - minX) // 2, (maxY - minY) // 2)
+
+img2[center[0]][center[1]] = [255, 0, 0]
+
+angle = sorted([[math.atan2(i[0] - center[0], i[1] - center[1]), dist(*i, *center)] for i in l])
+
+
+if __name__ == '__main__':
+    print(minX, maxX, minY, maxY)
+    print(center, '\n', r)
+    cv2.imwrite("output.png", img2)
+
+    pygame.init()
+    size = [800, 800]
+    screen = pygame.display.set_mode(size)
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        screen.fill([255, 255, 255])
+        color = [0, 255, 255]
+        for a, d in angle:
+            pygame.draw.line(screen, color, [400, 400], [400 + math.sin(a) * d, 400 + math.cos(a) * d], 1)
+
+        pygame.display.flip()
+    pygame.quit()
--- main.py
+++ main.py
@@ -2,19 +2,22 @@ from perlin_noise import PerlinNoise
 import pygame
 from math import sin, cos, pi
 from time import perf_counter
+import image_read
 
 
-def position(h, alpha):
+def position(h, alpha, angles=None):
     """
     :param h: list of distances from the center of a figure to points on its surface
     :param alpha: angle of rotation of the figure
+    :param angles:
     :return: coordinates of points on the surface of a figure relative to its center
     """
-    angle = [2 * pi * i / len(h) for i in range(len(h))]
-    return [[h[i] * sin(a + alpha), h[i] * cos(a + alpha)] for i, a in enumerate(angle)]
+    if angles is None:
+        angles = [2 * pi * i / len(h) for i in range(len(h))]
+    return [[h[i] * sin(a + alpha), h[i] * cos(a + alpha)] for i, a in enumerate(angles)]
 
 
-def draw(center_position, screen, pos, alpha, stretch=1.0):
+def draw(center_position, screen, pos, alpha, stretch=1.0, angles=None):
     """
     :param center_position: position of center of rotating figure
     :param screen: pygame screen
@@ -31,16 +34,28 @@ def draw(center_position, screen, pos, alpha, stretch=1.0):
 
     for i in range(len(height) - 1):
         color = (0, 0, 0)  # (0, i, 255)
-        pygame.draw.line(screen, color, [-dx * stretch + i * stretch * (2 * pi * r / n) + center_position[0], height[i] + center_position[1]],
-                         [-dx * stretch + i * stretch * (2 * pi * r / n) + 1 + center_position[0], height[i + 1] + center_position[1]], 1)
+        if angles is None:
+            a = i * (2 * pi / n)
+        else:
+            a = angles[i]
+        pygame.draw.line(screen, color, [-dx * stretch + a * r * stretch + center_position[0], height[i] + center_position[1]],
+                         [-dx * stretch + stretch * a * r + 1 + center_position[0], height[i + 1] + center_position[1]], 1)
 
 
-noiseGenerator = PerlinNoise()
-r = 100
-n = 255
-height = [r + noiseGenerator(i * 0.05) * 100 for i in range(n)]
-k = height[0] / height[-1]
-height = [height[i] * (1 + (k - 1) * (i / n)) for i in range(len(height))]
+random_generated = False
+if random_generated:
+    noiseGenerator = PerlinNoise()
+    r = 100
+    n = 255
+    height = [r + noiseGenerator(i * 0.05) * 100 for i in range(n)]
+    k = height[0] / height[-1]
+    height = [height[i] * (1 + (k - 1) * (i / n)) for i in range(len(height))]
+else:
+    r = image_read.r
+    n = len(image_read.angle)
+
+    height = [i[1] for i in image_read.angle]
+    angles = [i[0] for i in image_read.angle]
 
 pygame.init()
 
@@ -59,11 +74,15 @@ while run:
 
     screen.fill([255, 255, 255])
     alpha = (perf_counter() - t) * t_speed
-    pos = position(height, -alpha)
 
-    draw([100, 100], screen, pos, alpha,  1.0)
+    if random_generated:
+        pos = position(height, -alpha)
+    else:
+        pos = position(height, -alpha, angles=angles)
 
-    draw([100, 500], screen, pos, alpha, 1.1)
+    draw([500, 100], screen, pos, alpha,  1.0, angles=angles)
+
+    draw([500, 500], screen, pos, alpha, 1.1, angles=angles)
 
     pygame.display.flip()
 pygame.quit()
